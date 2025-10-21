c60fa5ceda47f5001f4d574404541398340dd117
Felipe Rughut <feliperughut@gmail.com>
2025-10-21 15:57:01 -0300

    adicionando-tela-funcionários

---
+++ Models/Fornecedores.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EmurbEstoque.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [StringLength(50)]
+        public string Nome { get; set; } = "";
+
+        [Required(ErrorMessage = "O campo CPF/CNPJ é obrigatório.")]
+        [StringLength(20)]
+        [Display(Name = "CPF/CNPJ")]
+        public string CpfCnpj { get; set; } = "";
+
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [StringLength(50)]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
+        public string Email { get; set; } = "";
+
+        [Required(ErrorMessage = "O campo Telefone é obrigatório.")]
+        [StringLength(14)]
+        public string Telefone { get; set; } = "";
+
+        [Required(ErrorMessage = "A Inscrição Estadual é obrigatória.")]
+        [StringLength(20)]
+        [Display(Name = "Inscrição Estadual")]
+        public string InscricaoEstadual { get; set; } = "";
+    }
+}
