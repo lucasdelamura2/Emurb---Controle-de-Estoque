231d1ef563f95e5d5a15d201d1176b620479d7cf
Felipe Rughut <feliperughut@gmail.com>
2025-10-28 21:26:52 -0300

    fazendo a conexão com o banco de dados

--- Program.cs
+++ Program.cs
@@ -2,25 +2,30 @@ using EmurbEstoque.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
 builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton<IFuncionarioRepository, FuncionarioMemoryRepository>();
+
+builder.Services.AddTransient<IFuncionarioRepository>(_ => new FuncionarioDatabaseRepository(connectionString));
+builder.Services.AddTransient<IProdutoRepository>(_ => new ProdutoDatabaseRepository(connectionString));
+
+// builder.Services.AddSingleton<IFuncionarioRepository, FuncionarioMemoryRepository>();
+// builder.Services.AddSingleton<IProdutoRepository, ProdutoMemoryRepository>(); 
 builder.Services.AddSingleton<IFornecedorRepository, FornecedorMemoryRepository>();
-builder.Services.AddSingleton<IProdutoRepository, ProdutoMemoryRepository>();
 builder.Services.AddSingleton<IOrdemEntradaRepository, OrdemEntradaMemoryRepository>();
 builder.Services.AddSingleton<ILoteRepository, LoteMemoryRepository>();
 builder.Services.AddSingleton<ILocalRepository, LocalMemoryRepository>();              
 builder.Services.AddSingleton<IAutorizadoRepository, AutorizadoMemoryRepository>();    
 builder.Services.AddSingleton<IAutorizacaoRepository, AutorizacaoMemoryRepository>();  
-builder.Services.AddSingleton<IOrdemSaidaRepository, OrdemSaidaMemoryRepository>();
+builder.Services.AddSingleton<IOrdemSaidaRepository, OrdemSaidaMemoryRepository>();    
 builder.Services.AddSingleton<IItensOSRepository, ItensOSMemoryRepository>();
 builder.Services.AddSingleton<IEstoqueRepository, EstoqueMemoryRepository>();
-builder.Services.AddSingleton<IPessoaRepository, PessoaMemoryRepository>();
-builder.Services.AddSingleton<IUsuarioRepository, UsuarioMemoryRepository>();
+
 
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Login/Error");
+    app.UseExceptionHandler("/Home/Error"); 
 }
 app.UseStaticFiles();
 
